dcf3d15c660ded21e6e8c5b1d3799bae98997b32
CNA-Bld <cna.bld@gmail.com>
2019-07-18 21:05:03 +0800

    Update JS dependencies

--- static/treediagram.user.js
+++ static/treediagram.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         TreeDiagram
 // @namespace    https://www.sshz.org/
-// @version      0.1.11.4
+// @version      0.1.12
 // @description  Make Eventernote Great Again
 // @author       SSHZ.ORG
 // @match        https://www.eventernote.com/*
 // @require      https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.bundle.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/0.5.7/chartjs-plugin-annotation.min.js
-// @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-zoom@0.7.0/dist/chartjs-plugin-zoom.min.js
+// @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-zoom@0.7.3/dist/chartjs-plugin-zoom.min.js
 // @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.6.0/dist/chartjs-plugin-datalabels.min.js
 // @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-colorschemes@0.4.0/dist/chartjs-plugin-colorschemes.min.js
 // ==/UserScript==
