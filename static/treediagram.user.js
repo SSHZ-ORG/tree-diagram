2e8816763ec7a94bc7bef46e4f677deb6993c189
CNA-Bld <cna.bld@gmail.com>
2020-01-30 13:43:35 +0800

    Update JS dependencies

--- static/treediagram.user.js
+++ static/treediagram.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         TreeDiagram
 // @namespace    https://www.sshz.org/
-// @version      0.1.12.1
+// @version      0.1.12.2
 // @description  Make Eventernote Great Again
 // @author       SSHZ.ORG
 // @match        https://www.eventernote.com/*
-// @require      https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.bundle.min.js
+// @require      https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.bundle.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/0.5.7/chartjs-plugin-annotation.min.js
-// @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-zoom@0.7.3/dist/chartjs-plugin-zoom.min.js
-// @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.6.0/dist/chartjs-plugin-datalabels.min.js
+// @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-zoom@0.7.5/dist/chartjs-plugin-zoom.min.js
+// @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0/dist/chartjs-plugin-datalabels.min.js
 // @require      https://cdn.jsdelivr.net/npm/chartjs-plugin-colorschemes@0.4.0/dist/chartjs-plugin-colorschemes.min.js
 // ==/UserScript==
 
@@ -135,6 +135,9 @@
                                 type: 'time',
                                 ticks: {
                                     maxRotation: 0,
+                                },
+                                gridLines: {
+                                    zeroLineColor: 'rgba(0, 0, 0, 0.1)',
                                 },
                             }],
                         },
@@ -329,6 +332,9 @@
                                 type: 'time',
                                 ticks: {
                                     maxRotation: 0,
+                                },
+                                gridLines: {
+                                    zeroLineColor: 'rgba(0, 0, 0, 0.1)',
                                 },
                             }],
                         },
